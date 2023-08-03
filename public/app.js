32c5d6cf613ef4552c92d14a7eb254aac8ca931a
kwojtkowiak <80783985+kwojtkowiak@users.noreply.github.com>
2023-08-03 12:01:59 +0200

    Fix errors and scope of app.ts

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
-import { ListTemplate } from './classes/listTemplate.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 const anchor = document.querySelector('a'); /*! to tell ts that it IS there*/
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector('#type'); /*it allows to use specific methods, as TS does not know whether this ID belongs to form/button/div etc*/
@@ -8,17 +8,20 @@ const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 const position = document.querySelector('#position');
-const positionValue = position.value;
 const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    const positionValue = position.value;
+    const detailsValue = details.value.trim() || 'nie podano szczegółów!';
+    let values;
+    values = [tofrom.value, detailsValue, amount.valueAsNumber, positionValue];
     let doc;
     if (positionValue === 'start' || positionValue === 'end') {
         doc =
             type.value === 'invoice'
-                ? new Invoice(tofrom.value, details.value, amount.valueAsNumber, positionValue)
-                : new Payment(tofrom.value, details.value, amount.valueAsNumber, positionValue);
+                ? new Invoice(...values) /*thanks to using tuple in line 25 I can use spread operator here (order of types is satisfied)*/
+                : new Payment(...values);
         list.render(doc, type.value, positionValue);
     }
 });
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { Invoice } from './classes/Invoice.js'
 import { Payment } from './classes/Payment.js'
-import { ListTemplate } from './classes/listTemplate.js'
+import { ListTemplate } from './classes/ListTemplate.js'
 import { HasFormatter } from './interfaces/HasFormatter.js'
 
 const anchor = document.querySelector('a')! /*! to tell ts that it IS there*/
@@ -13,21 +13,28 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement
 const details = document.querySelector('#details') as HTMLInputElement
 const amount = document.querySelector('#amount') as HTMLInputElement
 const position = document.querySelector('#position') as HTMLSelectElement
-const positionValue = position.value as 'start' | 'end'
 
 const ul = document.querySelector('ul')!
 const list = new ListTemplate(ul)
 
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault()
+const positionValue = position.value as 'start' | 'end'
+  
+  const detailsValue = details.value.trim() || 'nie podano szczegółów!'
+
+  let values: [string, string, number, string]
+  values = [tofrom.value, detailsValue, amount.valueAsNumber, positionValue]
 
   let doc: HasFormatter
 
   if (positionValue === 'start' || positionValue === 'end') {
     doc =
       type.value === 'invoice'
-        ? new Invoice(tofrom.value, details.value, amount.valueAsNumber, positionValue)
-        : new Payment(tofrom.value, details.value, amount.valueAsNumber, positionValue)
+        ? new Invoice(
+            ...values
+          ) /*thanks to using tuple in line 25 I can use spread operator here (order of types is satisfied)*/
+        : new Payment(...values)
 
     list.render(doc, type.value, positionValue)
   }
